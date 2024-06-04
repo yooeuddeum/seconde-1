1f41c298d9790cf8aab6ad427c16009783c48f86
유으뜸 <www.goolge.com>
2024-06-04 10:51:49 +0900

    에러의 상황과 아닌 경우

--- work-2.js
+++ work-2.js
@@ -1,9 +1,38 @@
+// class WorkTwo {
+//   constructor(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   }
+
+//   set a(value) {
+//     if (typeof value === "number") {
+//       return (this._a = value);
+//     } else {
+//       throw new Error("a는 숫자만 가능합니다.");
+//     }
+//   }
+// }
+
+// let test = new WorkTwo("qqq", 2);
+// console.log(test);
+
+
 class WorkTwo {
   constructor(a, b) {
     this.a = a;
     this.b = b;
   }
+
+  set a(value) {
+    if (typeof value === "number") {
+      return (this._a = value);
+    } else {
+      throw new Error("a는 숫자만 가능합니다.");
+    }
+  }
 }
 
 let test = new WorkTwo(1, 2);
 console.log(test);
+
+
