fbbf8524a4c6cb9ae475d262a0d48d525cfb1e2c
유으뜸 <www.goolge.com>
2024-06-04 10:35:49 +0900

    work-1 추가 작성

--- work-1.js
+++ work-1.js
@@ -1,8 +1,17 @@
+// function workOne(a, b) {
+//   if (typeof a === "number") {
+//     if (typeof b === "number") {
+//       let local = a + b;
+//       return local;
+//     }
+//   }
+// }
+
 function workOne(a, b) {
-  if (typeof a === "number") {
-    if (typeof b === "number") {
-      let local = a + b;
-      return local;
-    }
+  if (typeof a === "number" && typeof b === "number") {
+    let local = a + b;
+    return local;
+  }else {
+    console.log("매개변수 잘못적음");
   }
 }
