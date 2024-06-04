fb9947432d27f622966067ed3be0bc8514dfef7d
유으뜸 <www.goolge.com>
2024-06-04 11:47:42 +0900

    this 개념 파악

--- work-3.js
+++ work-3.js
@@ -6,3 +6,16 @@ function WorkThree(a, b) {
 }
 
 console.log(WorkThree(1, 2));
+
+let itsMe = {
+  name: "공욱재",
+  age: 26,
+  home: "대전",
+  test: this,
+  sentence: function () {
+    return this.name + "는 미남이다.";
+  },
+};
+
+console.dir("객체에서 보는 this :", itsMe);
+console.log("메서드로 보는 this", itsMe.sentence());
